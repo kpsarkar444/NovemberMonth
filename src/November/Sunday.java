8fb9c4364567c74a19e28255af3e91b5261d5b8b
prem sarkar <premsarkar2222@gmail.com>
2017-11-12 22:39:55 +0530

    hi

---
+++ src/November/Sunday.java
@@ -0,0 +1,12 @@
+package November;
+
+import org.testng.annotations.Test;
+
+public class Sunday {
+	@Test
+	public void m1() {
+		System.out.println("hello");
+		System.out.println("Hiiiiiiiii");
+	}
+
+}
